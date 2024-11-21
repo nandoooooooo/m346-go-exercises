97778126aeaa2c5de13a622daadc0dc31b473ada
Nando Schürmann <nando.schuermann@getabstract.com>
2024-11-21 15:46:48 +0100

    Exercise 1.1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,16 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+
+	firstName := "Nando"
+	lastName := "Schuermann"
+	dayOfBirth := 14
+	monthOfBirth := 2
+	yearOfBirth := 2008
+	numberOfSiblings := 1
+	heightInMeters := 1.83
+	zodiacSign := '\u2652'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
