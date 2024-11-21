b74bdb84bc720f03050cb1d54406fd4c0a20c4d1
Nando Schürmann <nando.schuermann@getabstract.com>
2024-11-21 16:21:04 +0100

    Exercise 2.1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	firstName string
+	lastName  string
 }
 
 // TODO: declare a structure for birth date
+type birthDate struct {
+	DayOfBirth   int
+	MonthOfBirth int
+	YearOfBirth  int
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	fullName         FullName
+	birthDate        birthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +26,19 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		fullName: FullName{
+			"Nando", "Schuermann",
+		},
+		birthDate: birthDate{
+			14, 2, 2008,
+		},
+		NumberOfSiblings: 1,        // TODO: adjust
+		ZodiacSign:       '\u2652', // TODO: adjust
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings += 1
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
