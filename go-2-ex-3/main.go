bfcd2f29de6f5fc1ae739cd79509dfe4841a62bf
Nando Schürmann <nando.schuermann@getabstract.com>
2024-11-28 16:16:15 +0100

    Exercise 2.3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -5,12 +5,17 @@ import "fmt"
 func main() {
 	// TODO: create a map called "modules"
 
+	modules := map[int]string{104: "Implement data models", 117: "Realise net infrastructure for a small company", 346: "Realise cloud solutions"}
+
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
 	// TODO: delete one
+	delete(modules, 117)
 	// TODO: add one
+	modules[320] = "OOP"
 	// TODO: replace one
+	modules[320] = "Teacher hopefully changes in close future"
 	fmt.Println(modules)
 }
