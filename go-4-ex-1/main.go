d9cce010a98dd7e58963b3e8a1e73ff40d8e3633
Nando Schürmann <nando.schuermann@getabstract.com>
2024-12-05 15:35:19 +0100

    Exercise 4.1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
 // TODO: implement the function computeGrade
 
 func main() {
 	// TODO: call the function computeGrade
+	fmt.Print(computeGrade(17.5, 28)) // 4.125
+	fmt.Print(computeGrade(29, 28))   // Error
+}
+
+func computeGrade(achievedPoints float64, maxPoints float64) (float64, error) {
+	if achievedPoints < 0 || maxPoints < 0 || achievedPoints > maxPoints {
+		return 0.0, fmt.Errorf("IllegalArgumentException: Achieved Points (%v) or Max Points (%v) is invalid", achievedPoints, maxPoints)
+	}
+	return achievedPoints/maxPoints*5 + 1, nil
 }
