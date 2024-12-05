99e514e0350b93ab6ec5c23db61c96aa6c94b09f
Nando Schürmann <nando.schuermann@getabstract.com>
2024-12-05 15:46:12 +0100

    Exercise 4.2

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+
+type ShortSides struct {
+	a float64
+	b float64
+}
 
 func main() {
 	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypotenuse(3, 4))
+	s1 := ShortSides{a: 5, b: 12}
+	fmt.Println(s1.Hypotenuse())
+}
+
+func computeHypotenuse(a, b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2)) // 5
+}
+
+func (s ShortSides) Hypotenuse() float64 {
+	return math.Sqrt(math.Pow(s.a, 2) + math.Pow(s.b, 2)) // 13
 }
